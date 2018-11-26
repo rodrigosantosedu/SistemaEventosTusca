9f5d078736c3a2697a5e6a0deb2d7699431a3ff4
Rodrigo Santos <rodrigobsi@usp.br>
2018-11-26 00:57:14 -0200

    Tela Inicial criada com os botoões de cadastrar e buscar

--- src/projetotusca/ProjetoTusca.java
+++ src/projetotusca/ProjetoTusca.java
@@ -1,8 +1,15 @@
 package projetotusca;
+import gui.TelaPrincipal;
 import java.sql.*;
+import javax.swing.JOptionPane;
 public class ProjetoTusca {
     private static String sql;
     public static void main(String[] args) throws Exception {
+        new TelaPrincipal().setVisible(true);
+    }
+    
+    // Essa função chama a conexão e faz uma consulta teste
+    public static void busca()throws Exception{
         Connection conexao = ObterConexao();
         Statement statement = conexao.createStatement();
         String query = "select nome,cnpj from "
@@ -10,11 +17,12 @@ public class ProjetoTusca {
         ResultSet resultSet = statement.executeQuery(query);
         
         while (resultSet.next()) {
+           // JOptionPane.showMessageDialog(TelaPrincipal.rootPane,"Chupa Federal");
             System.out.println(resultSet.getString("nome"));
             System.out.println(resultSet.getString("cnpj"));
         }
-
     }
+    
     private static Connection ObterConexao() {
         String user = "G7143510";
         String pass = "G7143510";
